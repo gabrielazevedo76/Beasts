9ed433906a353772126479d7763ce7ee8ef17c8a
somera3 <gabrielalmeida75@gmail.com>
2021-08-07 07:12:43 -0300

    Created folders, header and WildAnimals

---
+++ javascript/script.js
@@ -0,0 +1,38 @@
+const modalOverlay = document.querySelector(".modal-overlay")
+const wildImagesOpen = document.querySelectorAll(".wild_images")
+const modalContent = document.querySelector(".modal-content")
+
+
+wildImagesOpen.forEach(img => {
+    img.addEventListener("click", () => {
+        
+    })
+})
+
+
+
+
+for (let buyButton of buyButtons) {
+    buyButton.addEventListener("click", () => {
+        const buyButtonId = buyButton.getAttribute("id")
+        window.location.href = `/pacotes-info?id=${buyButtonId}`
+    })
+}
+
+
+
+for (let card of cards) {
+    card.addEventListener("click", function(){
+        modalOverlay.classList.add("active")
+        let cardImg = card.getAttribute("src")
+        if(modalOverlay.classList.contains("active")){
+            modalOverlay.querySelector("img").src = `${cardImg}`
+        }
+
+    })
+}
+
+document.querySelector(".close-modal").addEventListener("click", function(){
+    modalOverlay.classList.remove("active")
+    modalOverlay.querySelector("img").src = ``
+})
